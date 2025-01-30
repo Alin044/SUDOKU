58f2cc94c089ad4834122a800c259590757b823c
Alin <alinalin03030@gmail.com>
2025-01-30 00:10:38 +0000

    Generating a solvable sudoku grid

---
+++ script1.js
@@ -0,0 +1,26 @@
+
+function createRandomGrid(){
+    let grid = new Array(9).fill(null).map( () => new Array(9).fill(0));
+    
+    function getColumn(){
+        
+    }
+    function getInnerGrid(){
+
+    }
+
+    function isValid(x, y, newRand){
+        
+    }
+
+    for(let i = 0; i < 9; i++){
+        for(let j = 0; j < 9; j++){
+            let newRand = Math.floor(Math.random() * 9) + 1;
+            let attempts = 0;
+            do{
+                
+            }while(attempts < 100);
+        }
+    }
+
+}
